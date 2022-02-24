816cb6d41483d1cf197c0d36df5911ea277bb92c
كارل مبارك <bonjour@moubarak.eu>
2022-02-24 18:29:34 +0100

    backend: testig cors Access-Control-Expose-Headers

--- back/config/middlewares.js
+++ back/config/middlewares.js
@@ -1,11 +1,25 @@
 module.exports = [
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  // 'strapi::cors',
   {
     name: "strapi::cors",
     config: {
-      expose: ["WWW-Authenticate", "Server-Authorization", 'Content-Type', 'Authorization', 'Origin', 'Accept']
+      expose: [
+        // "WWW-Authenticate", 
+        // "Server-Authorization", 
+        // 'Content-Type', 
+        // 'Authorization', 
+        // 'Origin', 
+        // 'Accept',
+        "Content-Type",
+        "Authorization",
+        "X-Frame-Options",
+        "Origin",
+        "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Origin",
+        "Access-Control-Expose-Headers"
+      ]
     },
   },
   'strapi::poweredBy',
