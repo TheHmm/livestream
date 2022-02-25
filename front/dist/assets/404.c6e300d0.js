3f721c1da72e0ccab97c53d6f904b08f92f955ec
كارل مبارك <bonjour@moubarak.eu>
2022-02-25 08:34:29 +0100

    trying to use observer to watch for new assets in head

---
+++ front/dist/assets/404.c6e300d0.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.3d47ae1f.js";import{o as r,f as o}from"./vendor.5c5858dd.js";const c={};function t(a,n){return r(),o("main")}var s=e(c,[["render",t]]);export{s as default};
