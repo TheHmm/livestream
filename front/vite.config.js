a064c62662c5e8a2dec2991e7b0e330784a10ae6
كارل مبارك <bonjour@moubarak.eu>
2022-03-09 10:35:55 +0100

    vite coonfig: enabled sourcemap, manifest, removed module prelod poolyfill d chunksize limit is 100kb now

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,32 +1,30 @@
 import { fileURLToPath, URL } from 'url'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { visualizer } from "rollup-plugin-visualizer"
+import { defineConfig }       from 'vite'
+import vue                    from '@vitejs/plugin-vue'
+import { visualizer }         from "rollup-plugin-visualizer"
 
-// https://vitejs.dev/config/
+
+// reference: https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [
-    vue({
-      ssr: true
-    }),
-    visualizer( opts => ({
-      open: true,
-    })) 
+    vue(),
+    visualizer( opts => ( { open: true } ) ) 
   ],
   build: {
-    manifest: true,
-    sourcemap: true,
-    polyfillModulePreload: false, 
-    chunkSizeWarningLimit: 75,
+    manifest              : true,
+    sourcemap             : true,
+    polyfillModulePreload : false,
+    chunkSizeWarningLimit : 100,
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      // 'vue-router': "vue-router/dist/vue-router.esm-bundler.js",
-      // vue: "vue/dist/vue.esm-bundler.js",
-      moment: 'moment/src/moment',
-      'hls.js': 'hls.js/dist/hls.light.min.js',
-      'moment-timezone': 'moment-timezone/builds/moment-timezone-with-data-2012-2022.min.js'
+      '@'               : fileURLToPath(new URL('./src', import.meta.url)),
+      'vue-router'      : "vue-router/dist/vue-router.esm-bundler.js",
+      'vue'             : "vue/dist/vue.esm-bundler.js",
+      'moment'          : 'moment/src/moment',
+      'hls.js'          : 'hls.js/dist/hls.light.min.js',
+      'moment-timezone' : 'moment-timezone/builds/moment-timezone-with-data-2012-2022.min.js'
     }
   }
 })
