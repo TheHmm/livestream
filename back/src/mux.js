e0cc53104633a4898cdeab792923d02b5756ae57
كارل مبارك <bonjour@moubarak.eu>
2022-03-11 16:31:47 +0100

    ncaptions ad websockets in strapi

--- back/src/mux.js
+++ back/src/mux.js
@@ -59,6 +59,7 @@ module.exports = MUX_TOKEN => {
         seconds = asset?.recording_times[0]?.started_at?.seconds,
         nanos   = asset?.recording_times[0]?.started_at?.nanos,
         milli   = seconds * 1000 + nanos / 1000000
+        console.log(milli)
       return milli
     },
 
