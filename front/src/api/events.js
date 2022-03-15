ebd19c4205ac69e68f1e25d193d012b0789f0ecc
كارل مبارك <bonjour@moubarak.eu>
2022-03-15 09:21:31 +0100

    ui / semanntic html

--- front/src/api/events.js
+++ front/src/api/events.js
@@ -99,11 +99,9 @@ export default {
           'ends',
           'info',
         ],
-        populate: {
-          recording: {
-            fields: 'playback_id'
-          }
-        }
+        populate: [
+          'recording'
+        ]
         // fields: '*',
         // populate: [
         //   'logo',
