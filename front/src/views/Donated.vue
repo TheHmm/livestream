33f3b440dc8a737151e6763a254b9f59b75b99d0
كارل مبارك <bonjour@moubarak.eu>
2022-03-24 19:16:24 +0100

    addig from parameter to rediecert url

---
+++ front/src/views/Donated.vue
@@ -0,0 +1,43 @@
+<script>
+import Banner from '../components/Header/Banner.vue'
+
+export default { 
+  name: "Donated" ,
+  components: { Banner },
+}
+</script>
+
+<template>
+
+  <main>
+    <Banner />
+    <section>
+      <p>Thank you for donating to the Hmm!</p>
+      <p>
+        <router-link :to="$route.query.from">
+          Return to stream.
+        </router-link>
+      </p> 
+    </section>
+  </main>
+
+</template>
+
+<style scoped>
+main {
+  position: absolute;
+  width: 100%;
+}
+main #banner {
+  margin: 2rem auto;
+} 
+main section {
+  background: var(--white);
+  max-width: min(70%, 50rem);
+  margin: 5rem auto;
+  overflow: scroll;
+}
+main section p {
+  margin: 0;
+}
+</style>
