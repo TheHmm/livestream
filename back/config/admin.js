9bcc00e0161fc6df40be2cbdd8d8ac8c653bd469
Marco <marco@poop.nl>
2022-03-30 23:56:31 +0000

    add configs for url (from env)

--- back/config/admin.js
+++ back/config/admin.js
@@ -2,4 +2,5 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', 'b625f66afe16f882ef60b85a25903a16'),
   },
+  url: env('ADMINURL', ''),
 });
--- back/config/server.js
+++ back/config/server.js
@@ -1,6 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  proxy: env.bool('TRUST_PROXY', false),
+  url: env('URL', ''),
   app: {
     keys: env.array('APP_KEYS'),
   },
