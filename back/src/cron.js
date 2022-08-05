63ef60f2a2cec081363466e8bafb6a9d09b121f3
كارل مبارك <bonjour@moubarak.eu>
2022-08-05 20:06:39 +0200

    trying to re-connfigure event logic in frontend

--- back/src/cron.js
+++ back/src/cron.js
@@ -3,7 +3,7 @@ const random_animal_name = require("random-anonymous-animals")
 module.exports = {
 
   // '*/10 * * * * *': async ({ strapi }) => { // dev: every 3 seconds
-  '45 19 * * *': async ({ strapi }) => { // dev: every day at midnight
+  '07 20 * * *': async ({ strapi }) => { // dev: every day at midnight
   // '0 0 * * *': async ({ strapi }) => { // prod: every day at midnight
 
     strapi.log.info(`[ * * * * * * * * * * * * * * * * * * * ]`)
