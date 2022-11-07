382a994b9e6e49037b940f3b3a278ebdbd4ddbc2
كارل مبارك <bonjour@moubarak.eu>
2022-11-07 17:23:21 +0100

    cleaned up and commented code

--- back/src/mqtt.js
+++ back/src/mqtt.js
@@ -1,4 +1,5 @@
-// SOCKET.IO fuctions are here for better legibility
+// MQTT ; using this protocol to communicate with the ESP32
+// devices that are sometimes in the sapce.
 
 module.exports = ({ HOST, TOPIC }) => {
 
@@ -7,17 +8,43 @@ module.exports = ({ HOST, TOPIC }) => {
 
   const mqtt = require( 'mqtt' ).connect( HOST )
 
+
+  // shortcut to publish without always mentioning the TOPIC
+
   mqtt.send = payload => {
     mqtt.publish( TOPIC, payload )
   }
 
+
+  // Connect to host and subscribe to TOPIC
+
+  mqtt.on( 'connect', () => {
+    console.log( 'MQTT connected to host: ', HOST )
+    mqtt.subscribe( TOPIC )
+  })
+
+
+
+
+
+  // All the possible methods that an ESP32 module can execute
+  // on the server.
+
   mqtt.actions = {
 
-    say_hi( sender, data ) {
+
+    // acknowledge device existence and it's ability to send
+    // a payload
+
+    say_hi : function( sender, data ) {
       console.log( "Hello!", sender, data )
     },
 
-    connect( sender, data ) {
+
+    // connect to livestream socket io server and appear on
+    // front end as dot presenting as a viewer.
+
+    connect : function( sender, data ) {
       const uuid = `ESP-${ sender }`
       strapi.io.socket_viewer({
         name      : sender,
@@ -29,7 +56,10 @@ module.exports = ({ HOST, TOPIC }) => {
       }, 10 * 60 * 1000 )
     },
 
-    emoji( sender, emoji ) {
+
+    // send emoji to all
+
+    emoji : function( sender, emoji ) {
       const uuid = `ESP-${ sender }`
       strapi.io.socket_emoji({
         group : "global",
@@ -41,22 +71,22 @@ module.exports = ({ HOST, TOPIC }) => {
   }
 
 
-  mqtt.on( 'connect', () => {
-    console.log( 'MQTT connected to host: ', HOST )
-    mqtt.subscribe( TOPIC )
-    // let i = 0
-    // setInterval(() => {
-    //   mqtt.send( `server:say_hi:${ i }` )
-    //   i ++
-    //   if ( i == 10 ) {
-    //     i = 0
-    //   }
-    // }, 5000 )
-  })
+
+
+  // parse all received messages with the following template:
+  // message: "sender:action:data" where:
+  // sender is the name of the device
+  // action is one of the predefined "actions" methods
 
   mqtt.on( 'message', ( topic, payload ) => {
     const [ sender, action, data ] = payload.toString().split( ':' )
-    console.log( sender, action, data )
+    console.log( `${ sender }:${ action }:${ data }` )
+
+
+    // if the action is defined as a method in our actions,
+    // we call the method, passing the sender and payload,
+    // else we log this to the console.
+
     if ( mqtt.actions[action] ) {
       mqtt.actions[action]( sender, data )
     } else {
@@ -65,7 +95,12 @@ module.exports = ({ HOST, TOPIC }) => {
   })
 
 
-  // We return the mqtt object for use elsewhere.
+
+
+
+
+
+  // We return the MQTT object for use elsewhere.
 
   return mqtt
 
