4d6bd257ea01bc54a468b7a799e69ae08051dae7
كارل مبارك <bonjour@moubarak.eu>
2023-04-18 11:42:51 +0200

    scent: playing with timings

--- back/src/mqtt.js
+++ back/src/mqtt.js
@@ -113,8 +113,8 @@ module.exports = ({ HOST, TOPIC }) => {
       'hmmosphere'
     ],
 
-    DURATION: 0.6 * 60 * 1000,    // 3 mionutes
-    COMMAND_BEAT: 0.5 * 1000, // 2 seconds
+    DURATION: 10 * 1000,    // 3 mionutes
+    COMMAND_BEAT: 0.25 * 1000, // 2 seconds
 
 
     // the JS timeout to be able to send a deploy command
@@ -149,7 +149,7 @@ module.exports = ({ HOST, TOPIC }) => {
       mqtt.send( `server:scent:on` )              // activate scent diffuser
       await wait( this.COMMAND_BEAT )
       mqtt.send( `server:scent:long` )            // activate light
-      await wait( 12 * this.COMMAND_BEAT )
+      await wait( 10 * this.COMMAND_BEAT )
       mqtt.send( `server:scent:long` )            // activate light colorful
       await wait( this.DURATION )
       mqtt.send( `server:scent_power:off` )       // deactivate power relay
