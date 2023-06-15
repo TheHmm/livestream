3b310228b3ccaae5cca1e99b833286b2388b11c2
كارل مبارك <bonjour@moubarak.eu>
2023-06-15 19:21:09 +0200

    strapi update

--- back/config/admin.js
+++ back/config/admin.js
@@ -1,6 +1,9 @@
 module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET', 'b625f66afe16f882ef60b85a25903a16'),
-  },
   url: env('ADMINURL', ''),
+  auth: {
+    secret: env('ADMIN_JWT_SECRET'),
+  },
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
+  },
 });
