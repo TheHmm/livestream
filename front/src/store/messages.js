ecc10c6eee71a2954627037a7eab58f8ffa8c1f1
كارل مبارك <bonjour@moubarak.eu>
2026-02-04 12:41:57 +0100

    exclude messages with unknown senders

--- front/src/store/messages.js
+++ front/src/store/messages.js
@@ -110,7 +110,7 @@ export default {
         try {
           await dispatch( 'get_viewer', sender_id, { root: true } )
         } catch ( error ) {
-          $log.error( error )
+          return $log.error( error )
         }
       }
       
