6222dc9c20a63b55dfe747e18eb270997b41fcee
كارل مبارك <bonjour@moubarak.eu>
2026-04-14 15:44:08 +0200

    backend: extend jwt session duration

--- back/config/admin.js
+++ back/config/admin.js
@@ -2,6 +2,10 @@ module.exports = ({ env }) => ({
   url: env('ADMINURL', ''),
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
+    sessions: { 
+      accessTokenLifespan: 604800, // insecure
+      maxRefreshTokenLifespan: 2592000, // is default, but doesnt seem to work 
+    }
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
